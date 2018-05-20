4e019f47fd4aa38eacea517e90e0232ed84a64b3
puppycodes <alice@jodie.io>
2018-05-20 14:55:39 -0700

    refactor js includes and add parallax

---
+++ _includes/js/footer.js
@@ -0,0 +1,39 @@
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'fade',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+// forward slide
+
+  $( "#slider" ).click(function() {
+    mySwiper.slideNext();
+  });
+
+//ScrollMagic
+
+  $(function() {
+    // init controller
+  	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+  	// build scenes
+  	new ScrollMagic.Scene({triggerElement: "#parallax1"})
+  					.setTween("#parallax1 > .post", {y: "-150%", ease: Linear.easeNone})
+  					// .addIndicators()
+  					.addTo(controller);
+
+  	new ScrollMagic.Scene({triggerElement: "#parallax2"})
+  					.setTween("#parallax2 > .post", {y: "-100%", ease: Linear.easeNone})
+  					// .addIndicators()
+  					.addTo(controller);
+
+  	new ScrollMagic.Scene({triggerElement: "#parallax3"})
+  					.setTween("#parallax3 > .post", {y: "200%", ease: Linear.easeNone})
+  					// .addIndicators()
+  					.addTo(controller);
+  });
